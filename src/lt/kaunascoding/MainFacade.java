ef1f74c1743222e3219168723a2dddf4605bdfd2
Your Name <you@example.com>
2018-10-09 20:36:31 +0300

    file edit added

--- src/lt/kaunascoding/MainFacade.java
+++ src/lt/kaunascoding/MainFacade.java
@@ -7,6 +7,7 @@ public class MainFacade {
     public static void main(String[] args) {
         FileCRUD model = new FileCRUD(); // model nes pagal susitarima vadiname duomenis
         model.createFile();
+        model.updateFile();
     }
 }
 
