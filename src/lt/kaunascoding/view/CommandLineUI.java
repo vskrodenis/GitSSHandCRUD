4c236c595981ea15dc27043c85f79435611ddbd6
Your Name <you@example.com>
2018-10-12 19:37:09 +0300

    Pridetas MVC

---
+++ src/lt/kaunascoding/view/CommandLineUI.java
@@ -0,0 +1,13 @@
+package lt.kaunascoding.view;
+//View dalyje esantis kodas bendrauja su vartotoju
+public class CommandLineUI {
+     public void showMeniu() {
+        System.out.println("Iveskite pasirinkima");
+        System.out.println("1- nurodyti failo varda");
+        System.out.println("2- sukurti faila");
+        System.out.println("3- papildyti faila tekstu");
+        System.out.println("4- parodyti failo turini");
+        System.out.println("5- trinti faila");
+        System.out.println("0- baigti darba");
+    }
+}
